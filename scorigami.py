83318e22d2e39b82c8b8d8fefce67fde7deff2fb
Cole Vahey <rcvahey01@gmail.com>
2018-11-20 19:32:04 -0500

    Add files via upload

---
+++ scorigami.py
@@ -0,0 +1,65 @@
+import csv
+from table import games_table
+
+csv_file = open('saved.csv')
+past_scores = csv.DictReader(csv_file)
+
+class Location:
+    numOccurred = 0
+
+    def __init__(self, winnerPoints, loserPoints, gameNum):
+        self.winnerPoints = winnerPoints
+        self.loserPoints = loserPoints
+        self.gameNum = gameNum
+
+    def setNumber(self, num):
+        self.numOccurred = num
+
+    def checkPossible(self):
+        if self.winnerPoints >= self.loserPoints:
+            return True
+        else:
+            # RETURN FALSE
+            return False
+
+    # COLORS: \033[48;5;39m `BLUE` --> \033[48;5;34m `GREEN`
+    def setColor(self):
+        if self.checkPossible():
+            if self.numOccurred > 25:
+                colNum = 22
+            elif 12 < self.numOccurred <= 25:
+                colNum = 28
+            elif 5 < self.numOccurred <= 12:
+                colNum = 34
+            elif 3 < self.numOccurred <= 5:
+                colNum = 40
+            elif 1 < self.numOccurred <= 3:
+                colNum = 46
+            elif 0 < self.numOccurred <= 1:
+                colNum = 82
+            else:
+                colNum = 230
+        else:
+            colNum = 232
+        return f"\033[48;5;{colNum}m   \033[0m"
+
+
+positions = []
+# n is row number (loser score)
+for n in range(76):
+    # i is the game number
+    for i in range(n*76,(n+1)*76):
+        positions.append(Location((i-76*n),n, i))
+
+for score in past_scores:
+    for position in positions:
+        if position.winnerPoints == int(score["PtsW"]) and position.loserPoints == int(score["PtsL"]):
+            print(position.winnerPoints, position.loserPoints, sep="-")
+            position.setNumber(int(score["Count"]))
+
+
+positions = [position.setColor() for position in positions]
+
+input("Press [ENTER] to continue")
+print("\033[H\033[2J")
+print(games_table.format(*positions))
